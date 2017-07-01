267cc61f12ca819e3ee8963f50233d38feca1962
shankyb9 <sbhalotia9@gmail.com>
2017-07-01 19:38:55 +0000

    Setting initial layout

---
+++ data.py
@@ -0,0 +1,26 @@
+def Articles():
+	articles = [
+	{
+		'id':1,
+		'title':'Article One',
+		'body':'Flask is called a micro framework because it does not require particular tools or libraries.[6] It has no database abstraction layer, form validation, or any other components where pre-existing third-party libraries provide common functions. ',
+		'author':'Wikipedia',
+		'create_date':'07-01-2017'
+	},
+	{
+		'id':2,
+		'title':'Article Two',
+		'body':'Flask is called a micro framework because it does not require particular tools or libraries.[6] It has no database abstraction layer, form validation, or any other components where pre-existing third-party libraries provide common functions. ',
+		'author':'Wikipedia',
+		'create_date':'07-01-2017'
+	},
+	{
+		'id':3,
+		'title':'Article Three',
+		'body':'Flask is called a micro framework because it does not require particular tools or libraries.[6] It has no database abstraction layer, form validation, or any other components where pre-existing third-party libraries provide common functions. ',
+		'author':'Wikipedia',
+		'create_date':'07-01-2017'
+	},
+	]
+
+	return articles
