de318d093bd6747321bfcd4299e8ab0032de527c
shankyb9 <sbhalotia9@gmail.com>
2017-07-02 02:59:55 +0000

    Adding articles and dashboard functioning

--- app.py
+++ app.py
@@ -1,31 +1,233 @@
-from flask import Flask, render_template
-from data import Articles
+from flask import Flask, render_template, flash, redirect, request, url_for, session, logging
+# from data import Articles
+from flask_mysqldb import MySQL
+from wtforms import Form, StringField, TextAreaField, PasswordField, validators
+from passlib.hash import sha256_crypt
+from functools import wraps
 
 app = Flask(__name__)
 #app.debug = True
 
-Articles = Articles()
+# Config MySQL
+app.config['MYSQL_HOST'] = 'localhost' 
+app.config['MYSQL_USER'] = 'root' 
+app.config['MYSQL_PASSWORD'] = 'shanky' 
+app.config['MYSQL_DB'] = 'myflaskapp' 
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+
+# Initialise sql
+mysql = MySQL(app) 
+
+
+# Articles = Articles()
 
 @app.route('/')
 def index():
 	return render_template('home.html')
 	#return 'INDEX'
 
+# About
 @app.route('/about')
 def about():
 	return render_template('about.html')
 
+# Articles
 @app.route('/articles')
 def articles():
-	return render_template('articles.html', articles=Articles)
+	# Create cursor
+	cur = mysql.connection.cursor()
 
+	# Get articles
+	result = cur.execute("SELECT * FROM articles")
+
+	articles = cur.fetchall()
+
+	if result > 0:
+		return render_template('articles.html', articles=articles)
+	else:
+		msg = 'No articles found!'
+		return render_template('articles.html', msg=msg)
+	# Close connection
+	cur.close()
+			
+	
 # using Jinja template engine
 
+# Single article
 @app.route('/article/<string:id>')
 def article(id):
-	return render_template('article.html', id=id)
+	# Create cursor
+	cur = mysql.connection.cursor()
+
+	# Get articles
+	result = cur.execute("SELECT * FROM articles WHERE id=%s",[id])
+
+	article = cur.fetchone()
+	return render_template('article.html', article=article)
+
+
+# define forms
+class RegisterForm(Form):
+    name = StringField('Name', [validators.Length(min=1, max=50)])
+    username = StringField('UserName', [validators.Length(min=4, max=25)])
+    email = StringField('Email', [validators.Length(min=6, max=50)])
+    password = PasswordField('Password', [
+    	validators.DataRequired(),
+    	validators.EqualTo('confirm', message='Passwords do not match')
+    	])
+    confirm = PasswordField('Confirm Password')
+
+
+# User Register
+@app.route('/register', methods=['GET', 'POST'])    
+def register():
+	form = RegisterForm(request.form)
+	if(request.method == 'POST' and form.validate()):
+		name = form.name.data
+		email = form.email.data
+		username = form.username.data
+		password = sha256_crypt.encrypt(str(form.password.data))
+
+		# Create cursor
+		cur = mysql.connection.cursor()
+
+		cur.execute("INSERT INTO users(name, email, username, password) VALUES (%s, %s, %s, %s)", (name, email, username, password))
+
+		# Commit to DB
+		mysql.connection.commit()
+
+		# Close connection
+		cur.close()
+
+		flash('You are now registered and can log in!','success')
+		return redirect(url_for('login'))
+
+	return render_template('register.html', form = form)
+
+
+# User Login
+@app.route('/login', methods=['GET','POST'])
+def login():
+	if request.method == 'POST':
+		# Fet form fields
+		# not using wtforms as not really needed here
+		username = request.form['username']
+		password_candidate = request.form['password']
+
+		# Create cursor
+		cur = mysql.connection.cursor()
+
+		# Get user by username
+		result = cur.execute("SELECT * FROM users 	where username = %s", [username])
+		if result > 0:
+			# Get stored hash
+			data = cur.fetchone()
+			password = data['password']
+
+			# Compare passwords
+			if sha256_crypt.verify(password_candidate, password):
+				# Can log in
+				session['logged_in'] = True
+				session['username'] = username
+
+				flash('You are now logged in!', 'success')
+				return redirect(url_for('dashboard'))
+
+			else:
+				error = 'Invalid password'
+				return render_template('login.html', error=error)
+
+			cur.close()	
+			
+		else:
+			error = 'Username not found'
+			return render_template('login.html', error=error)
+			#app.logger.info('No user')
+						
+
+	return render_template('login.html')
+
+
+# Check if user is logged in
+def is_logged_in(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return f(*args, **kwargs)
+        else:
+         	flash('Unauthorized! Please Login first!','danger')
+         	return redirect(url_for('login'))
+    return wrap     	
+
+
+# Logout
+@app.route('/logout')
+@is_logged_in
+def logout():
+	session.clear()
+	flash('You are now logged out', 'success')
+	return redirect(url_for('login'))
+
+
+# Dashboard
+@app.route('/dashboard')
+@is_logged_in
+def dashboard():
+	# Create cursor
+	cur = mysql.connection.cursor()
+
+	# Get articles
+	result = cur.execute("SELECT * FROM articles")
+
+	articles = cur.fetchall()
+
+	if result > 0:
+		return render_template('dashboard.html', articles=articles)
+	else:
+		msg = 'No articles found!'
+		return render_template('dashboard.html', msg=msg)
+	# Close connection
+	cur.close()
+			
+
+	return render_template('dashboard.html')
+
+
+# Article form class
+class ArticleForm(Form):
+    title = StringField('Title', [validators.Length(min=1, max=200)])
+    body = TextAreaField('Body', [validators.Length(min=10)])
+    
+
+
+# Add article
+@app.route('/add_article', methods=['GET', 'POST'])
+@is_logged_in
+def add_article():
+	form = ArticleForm(request.form)
+	if(request.method == 'POST' and form.validate()):
+		title = form.title.data
+		body = form.body.data
+
+		# Create cursor
+		cur = mysql.connection.cursor()
+
+		cur.execute("INSERT INTO articles(title, body, author) VALUES (%s, %s, %s)", (title, body, session['username']))
+
+		# Commit to DB
+		mysql.connection.commit()
+
+		# Close connection
+		cur.close()
+
+		flash('Your article is added successfully!')
+		return redirect(url_for('dashboard'))
+
+	return render_template('add_article.html', form = form)
 
 
 
 if(__name__ == '__main__'):
+	# adding secret is imp.
+	app.secret_key='secret123'
 	app.run(debug=True)
